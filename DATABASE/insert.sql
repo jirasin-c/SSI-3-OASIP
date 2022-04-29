f6271e7661d0be6b68465d9da1b1cc8c998df24f
bamnpsb9577 <bam.infinite@gmail.com>
2022-04-29 22:33:01 +0700

    add create script DB and insert script DB

---
+++ DATABASE/insert.sql
@@ -0,0 +1,16 @@
+use oasip;
+insert into eventcategory values
+(1,'Project Management Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย project management clinic ในวิชา INT221 integrated project I ให้นักศึกษาเตรียมเอกสารที่เกี่ยวข้องเพื่อแสดงระหว่างขอคำปรึกษา',30);
+insert into eventcategory values
+(2,' DevOps/Infra Clinic','Use this event category for DevOps/Infra clinic.',20);
+insert into eventcategory values
+(3,' Database Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย database clinic ในวิชา INT221 integrated project I',15);
+insert into eventcategory values
+(4,' Client-side Clinic','ตารางนัดหมายนี้ใช้สำหรับนัดหมาย client-side clinic ในวิชา INT221 integrated project I',30);
+insert into eventcategory values
+(5,' Server-side Clinic','',30);
+
+
+insert into event values(1,'Somchai Jaidee(OR-7)','somchai.jai@mail.kmutt.ac.th','22-05-23 13:30:00',30,'',2);
+insert into event values(2,'Somsri Rakdee(SJ-3)','somsri.rak@mail.kmutt.ac.th','22-04-27 09:30:00',30,'ขอปรึกษาปัญหาเพื่อนไม่ช่วยงาน',1);
+insert into event values(3,'สมเกียรติ ขยันเรียน กลุ่ม TT-4','somkiat.kay@kmutt.ac.th','22-05-23 16:30:05',15,'',3);
