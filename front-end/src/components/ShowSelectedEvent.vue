0d549ba553073fe046d0c45cb636e9a318e9cc7a
kewkewekw007 <jirasin4826@gmail.com>
2022-05-02 04:41:58 +0700

    add Detail events and fix UI

--- front-end/src/components/ShowSelectedEvent.vue
+++ front-end/src/components/ShowSelectedEvent.vue
@@ -9,19 +9,24 @@ const prop = defineProps({
     require: true,
   },
 });
+defineEmits(['clear'])
 
 // const eventCategory = prop.eventDetail.eventCategoryID.eventCategoryName;
 </script>
 
 <template>
   <div>
-    <div class="container mx-auto bg-white">
-      {{ prop.eventDetail.bookingName }}<br>
-      {{ prop.eventCategory }}<br>
-      {{ prop.eventDetail.eventStartTime }}<br>
-      {{ prop.eventDetail.eventDuration }}
+    <div class="text-2xl mx-auto text-center pt-72">
+      <b>Name: {{ prop.eventDetail.bookingName }}</b><br>
+      <b>Category: {{ prop.eventCategory }}</b>
+      <p>Date: {{ prop.eventDetail.eventStartTime }}</p>
+      <p>Duration: {{ prop.eventDetail.eventDuration }}</p>
+      <p>Email: {{ prop.eventDetail.bookingEmail }}</p>
+      <p v-show="prop.eventDetail.eventNotes != ''">Notes: {{ prop.eventDetail.eventNotes }}</p>
+      <button class="rounded-xl bg-blue-200 w-24 mt-10" @click="$emit('clear', $event)">Clear</button>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+</style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -8,8 +8,12 @@ const selectedEvent = ref([]);
 const getEventCategoryName = ref("");
 
 const getEvent = async () => {
-  const res = await fetch("http://localhost:3000/api/event");
+  const res = await fetch("http://localhost:3000/api/events");
   event.value = await res.json();
+  event.value.filter((e) => {
+    const localDate = new Date(e.eventStartTime).toLocaleString('th-TH', { weekday: "short", month: "short", day: "numeric", year: "2-digit", hour: "numeric", minute: "numeric" })
+    e.eventStartTime = localDate;
+  });
   if (event.value.length == 0) {
     isEmpty.value = true;
   } else {
@@ -18,24 +22,23 @@ const getEvent = async () => {
   console.log(isEmpty.value);
 };
 
-const convertToLocalDate = async () => {
-  event.value.filter((e) => {
-    const localDate = new Date(e.eventStartTime).toLocaleString();
-    e.eventStartTime = localDate;
-  });
-};
 
 const getDetailById = async (eventId) => {
-  const res = await fetch(`http://localhost:3000/api/event/${eventId}`);
+  const res = await fetch(`http://localhost:3000/api/events/${eventId}`);
   selectedEvent.value = await res.json();
   getEventCategoryName.value =
     selectedEvent.value.eventCategoryID.eventCategoryName;
+  const localDate = new Date(selectedEvent.value.eventStartTime).toLocaleString('th-TH', { weekday: "short", month: "short", day: "numeric", year: "2-digit", hour: "numeric", minute: "numeric" })
+  selectedEvent.value.eventStartTime = localDate;
 };
 
+const clearDetail = () => {
+  selectedEvent.value = ''
+}
 onBeforeMount(async () => {
   await getEvent();
-  await convertToLocalDate();
 });
+
 </script>
 
 <template>
@@ -44,32 +47,26 @@ onBeforeMount(async () => {
       <div class="mt-10 ml-16 text-2xl">CURRENT BOOKING</div>
     </div>
     <div class="flex flex-row h-screen">
-      <div
-        class="bg-gray-200 w-5/6 h-3/4 ml-16 mt-12 mr-16 rounded-2xl overflow-auto"
-      >
+      <div class="bg-gray-200 w-5/6 h-3/4 ml-16 mt-12 mr-16 rounded-2xl overflow-auto">
         <div v-show="isEmpty" class="grid justify-items-center pt-72">
           <p class="text-2xl text-gray-400">List currently empty</p>
         </div>
 
-        <div
-          class="columns-3 gap-6 w-[1100px] mx-auto space-y-6 pb-28 text-2xl mt-10"
-          id="style-1"
-        >
+        <div class="columns-3 gap-6 w-[1100px] mx-auto space-y-6 pb-28 text-2xl mt-10" id="style-1">
           <EventCard :allBooking="event" @viewDetail="getDetailById" />
         </div>
       </div>
 
       <div class="bg-gray-200 w-2/6 h-3/4 mt-12 mr-16 rounded-2xl">
-        <p
-          v-if="selectedEvent.length == 0"
-          class="text-center pt-72 text-2xl text-gray-400"
-        >
+        <p v-if="selectedEvent.length == 0" class="text-center pt-72 text-2xl text-gray-400">
           Please select some event.
         </p>
-        <ShowSelectedEvent :eventDetail="selectedEvent" :eventCategory="getEventCategoryName" />
+        <ShowSelectedEvent v-else :eventDetail="selectedEvent" :eventCategory="getEventCategoryName"
+          @clear="clearDetail" />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
