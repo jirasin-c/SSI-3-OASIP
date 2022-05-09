03327f735385b7469147b66ef8649f3aaf011815
kewkewekw007 <jirasin4826@gmail.com>
2022-05-09 22:02:36 +0700

    edit dockerfile and docker-compose

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080/",
+        // target: "http://localhost:8080/",
+        target: "http://10.4.56.114:8080/",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^/api/, '')
