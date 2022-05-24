f55e20b106d549dee8cb77af3ddbf54fa65dd5a8
bamnpsb9577 <bam.infinite@gmail.com>
2022-05-24 07:02:49 +0700

    edit list date latest

--- BACKEND/src/main/java/sit/ssi3/oasip/services/EventService.java
+++ BACKEND/src/main/java/sit/ssi3/oasip/services/EventService.java
@@ -107,16 +107,20 @@ public class EventService {
     public List<EventDTO> getListDay(Date dateEvent, String sortBy) {
         if (dateEvent == null)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "dateEvent is null");
-        System.out.println("kuy : " + dateEvent);
-        Date specifiedDate = new Date(dateEvent.getTime());
-        System.out.println("hee : " + specifiedDate);
+
+        long year = 17134650000000L;
+        Date specifiedDate = new Date();
+        specifiedDate.setTime(dateEvent.getTime()+year);
+        Date endTime = new Date();
+        endTime.setTime(specifiedDate.getTime() + 86400000 );
+
         // find all event
         List<Event> eventList = eventRepository.findAll(Sort.by(sortBy).ascending());
 
         // check event
-        eventList = eventList.stream().filter(allEvent -> {
+        eventList = eventList.stream().filter((allEvent) -> {
             Date allStartTime = new Date(allEvent.getEventStartTime().getTime());
-            return specifiedDate.compareTo(allStartTime) == 0;
+            return ((allStartTime.compareTo(specifiedDate) > 0) && (allStartTime.compareTo(endTime) < 0));
         }).collect(Collectors.toList());
 
         //Exception handling
